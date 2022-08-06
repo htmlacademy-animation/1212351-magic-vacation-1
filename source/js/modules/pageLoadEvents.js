22407bed5ba7177bbaa4ded6d7c1d6e82c221397
KirillVA20 <bcfe66e@gmail.com>
2022-08-06 23:11:38 +0300

    first commit

---
+++ source/js/modules/pageLoadEvents.js
@@ -0,0 +1,18 @@
+/* A function that takes a list of selectors and removes the hidden attribute from the elements that match the selectors.
+ * */
+export const switchLoadClass = (type, selectorList, className) => {
+  selectorList.forEach((selector) => {
+    const nodeElement = document.querySelector(selector);
+    if (type === `add`) {
+      nodeElement.classList[type](className);
+    }
+  });
+};
+
+const loadSelectorList = [`body`];
+
+/* Adding an event listener to the window object */
+export const pageLoadEvents = () => {
+  window.addEventListener(`load`, () => switchLoadClass(`add`, loadSelectorList, `load`));
+};
+
